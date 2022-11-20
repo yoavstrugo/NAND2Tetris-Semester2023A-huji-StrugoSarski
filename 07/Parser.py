9bab247269862cb39e62312025f2bed51bea48f9
Yoav Strugo <yoav.strugo.44@gmail.com>
2022-11-20 14:14:12 +0200

    parser implemeted

--- 07/Parser.py
+++ 07/Parser.py
@@ -5,8 +5,28 @@ was written by Aviv Yaish. It is an extension to the specifications given
 as allowed by the Creative Common Attribution-NonCommercial-ShareAlike 3.0
 Unported [License](https://creativecommons.org/licenses/by-nc-sa/3.0/).
 """
-import typing
+from dataclasses import dataclass
+from typing import Dict, Tuple, List, TextIO
 
+CMD : Dict[str, str] = {
+        'add': 'C_ARITHMETIC',
+        'sub': 'C_ARITHMETIC',
+        'neg': 'C_ARITHMETIC',
+        'eq': 'C_ARITHMETIC',
+        'gt': 'C_ARITHMETIC',
+        'lt': 'C_ARITHMETIC',
+        'and': 'C_ARITHMETIC',
+        'or': 'C_ARITHMETIC',
+        'not': 'C_ARITHMETIC',
+        'push': 'C_PUSH',
+        'pop': 'C_POP',
+        'label': 'C_LABEL',
+        'goto': 'C_GOTO',
+        'if': 'C_IF',
+        'function': 'C_FUNCTION',
+        'return': 'C_RETURN',
+        'call': 'C_CALL',
+    }
 
 class Parser:
     """
@@ -46,7 +66,22 @@ class Parser:
       - return
     """
 
-    def __init__(self, input_file: typing.TextIO) -> None:
+    @dataclass
+    class VMLine:
+        command: str
+        arg1: str
+        arg2: str
+
+    @staticmethod
+    def parse_line(line : str) -> Tuple[str, str, str]:
+        splitted_line = line.split(' ')
+        command = CMD[splitted_line[0]] 
+        if command == 'C_ARITHMETIC':
+            return Parser.VMLine(command=command, arg1=splitted_line[0], arg2=None)
+        else:
+            return Parser.VMLine(command=command, arg1=splitted_line[1], arg2=splitted_line[2])
+
+    def __init__(self, input_file: TextIO) -> None:
         """Gets ready to parse the input file.
 
         Args:
@@ -54,8 +89,19 @@ class Parser:
         """
         # Your code goes here!
         # A good place to start is to read all the lines of the input:
-        # input_lines = input_file.read().splitlines()
-        pass
+        input_lines = input_file.read().splitlines()
+        # Save (index, line) for each line
+        indexed_lines : List[Tuple[int, str]] = list(enumerate(input_lines))
+
+        # Remove comments and empty rows
+        indexed_lines = list(filter(lambda line: line[1] and ('//' not in line[1]) and (not line[1].isspace()), indexed_lines))
+
+        # Parse all lines
+        self.parsed_lines : List[Tuple[int, self.VMLine]] = list(map(lambda line: (line[0], self.parse_line(line[1])), indexed_lines))
+
+        self.line_selector = 0
+
+        print(self.parsed_lines)
 
     def has_more_commands(self) -> bool:
         """Are there more commands in the input?
@@ -64,7 +110,7 @@ class Parser:
             bool: True if there are more commands, False otherwise.
         """
         # Your code goes here!
-        pass
+        return self.line_selector < len(self.lines)
 
     def advance(self) -> None:
         """Reads the next command from the input and makes it the current 
@@ -72,7 +118,7 @@ class Parser:
         there is no current command.
         """
         # Your code goes here!
-        pass
+        self.line_selector += 1
 
     def command_type(self) -> str:
         """
@@ -84,7 +130,7 @@ class Parser:
             "C_RETURN", "C_CALL".
         """
         # Your code goes here!
-        pass
+        return self.parsed_lines[self.line_selector][1].command
 
     def arg1(self) -> str:
         """
@@ -94,7 +140,7 @@ class Parser:
             Should not be called if the current command is "C_RETURN".
         """
         # Your code goes here!
-        pass
+        return self.parsed_lines[self.line_selector][1].arg1
 
     def arg2(self) -> int:
         """
@@ -104,4 +150,4 @@ class Parser:
             "C_FUNCTION" or "C_CALL".
         """
         # Your code goes here!
-        pass
+        return self.parsed_lines[self.line_selector][1].arg2
